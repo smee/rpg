446ea1b93778afcd50fc52b9d9c7c04b0ad90f39
Oliver Flasch <oflasch@alloy.local>
2010-03-04 21:19:46 +0100

    added an example for using the new symbolic regression interface

---
+++ examples/appTempRGP1.r
@@ -0,0 +1,21 @@
+## appTempRGP1.r
+## First symbolic regression experiments on the AppTemperature data, no embedding
+## 2010 Oliver Flasch
+##
+
+## Data loading and partitioning (1/6 training, 5/6 test)...
+load("~/data/appTemperatureStreet1Pool5_clean.rda")
+trainingData <- subDataFrame(appTemperatureStreet1Pool5, size = 1/6, pos = "START")
+testData <- subDataFrame(appTemperatureStreet1Pool5, size = 5/6, pos = "END")
+
+## Evolution (time budget: 8 hrs)
+population <- symbolicRegression(NH4N ~ Temperature + pH + Conductivity, trainingData,
+                                 stopCondition = makeTimeStopCondition(8 * 60 * 60))
+
+## Result analysis...
+trainingFitnessFunction <- makeRegressionFitnessFunction(NH4N ~ Temperature + pH + Conductivity, trainingData)
+summary(popfitness(population, trainingFitnessFunction)) # RMSE on training data
+
+testFitnessFunction <- makeRegressionFitnessFunction(NH4N ~ Temperature + pH + Conductivity, testData)
+summary(popfitness(population, testFitnessFunction)) # RMSE on testing data
+sortBy(population, testFitnessFunction)[[1]] # best individual on training data
