00868eb1424e331e050f62edb21fd4aeecbe60b9
Oliver Flasch <oflasch@p046086.public-gm.fh-koeln.de>
2010-03-09 17:28:07 +0100

    a bit of refactoring, added functions for detecting phenotype-constant individuals

---
+++ examples/rgp.r
@@ -0,0 +1,24 @@
+# rgp.r
+# A simple script that sources all components of the RGP system to simplify
+# interactive development.
+# 2010 Oliver Flasch, released under the GPL v2
+#
+
+
+source("../skel/R/list_utils.r")
+source("../skel/R/expression_utils.r")
+source("../skel/R/function_utils.r")
+source("../skel/R/plot_utils.r")
+source("../skel/R/data_utils.r")
+source("../skel/R/stypes.r")
+source("../skel/R/search_space.r")
+source("../skel/R/creation.r")
+source("../skel/R/individual.r")
+source("../skel/R/similarity.r")
+source("../skel/R/population.r")
+source("../skel/R/fitness.r")
+source("../skel/R/complexity.r")
+source("../skel/R/selection.r")
+source("../skel/R/mutation.r")
+source("../skel/R/recombination.r")
+source("../skel/R/evolution.r")
