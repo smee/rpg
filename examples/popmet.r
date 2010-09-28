da6feeef2cb73a39216f04cb5a4c0e00276eeba9
Oliver Flasch <oflasch@p040119.vpn-f10.fh-koeln.de>
2010-09-28 17:08:48 +0200

    added an example for MDS based GP population visualization and an example for genotypic distance based Ward Clustering of GP populations

---
+++ examples/popmet.r
@@ -0,0 +1,42 @@
+# popmet.r
+# Genotypic distance metrics for GP, test cases
+# 2010 Oliver Flasch
+#
+
+require(rgp)
+require(MASS)
+
+populationDistanceMatrix <- function(p) {
+  d <- normInducedFunctionDistance(exprVisitationLength)
+  customDist(p, d)
+}
+
+populationPlotMDS <- function(p) {
+  dm <- populationDistanceMatrix(p)
+  dmp <- abs(dm + rnorm(1, sd = 0.01)) # pertube and remove negative values
+  fit <- isoMDS(dmp, k=2)
+  plot(fit$points[,1], fit$points[,2], main="Nonmetric MDS of Genotypic Distance")
+}
+
+populationWardClustering <- function(p, k = 5) {
+  d <- populationDistanceMatrix(p)
+  fit <- hclust(d, method = "ward")
+  cutree(fit, k)
+}
+
+populationPlotWardClusteringDendrogram <- function(p, k = 5) {
+  groups <- populationWardClustering(p, k) 
+  plot(fit)
+  rect.hclust(fit, k, border = "red")
+}
+
+populationPlotWardClusteringMDS <- function(p, k = 5) {
+  dm <- populationDistanceMatrix(p)
+  dmp <- abs(dm + rnorm(1, sd = 0.01)) # pertube and remove negative values
+  groups <- cutree(hclust(dm, method = "ward"), k)
+  fit <- isoMDS(dmp, k=2)
+  x <- fit$points[,1]
+  y <- fit$points[,2]
+  plot(x, y, type = "n", main = "Ward Clustering by Genotypic Distance, Nonmetric MDS")
+  text(x, y, labels = groups, cex=.7)
+}
