55cc23cdc63ce5e7f0a65a74bc80d0b502d3e382
Oliver Flasch <oflasch@pc-14-117.ubdointern>
2010-12-06 15:54:39 +0100

    Bugfix to typedGeneticProgramming.r. It is a really bad idea to bind attributes to R symbols (that where generated by as.name().

--- examples/typedGeneticProgramming.r
+++ examples/typedGeneticProgramming.r
@@ -61,22 +61,22 @@ vectorDimension <- 3
 `%s^%` <- function(x, y) x ^ y
 `%v^%` <- function(x, y) x ^ y
 
-typedSvmKernelConstantSet <- constantFactorySet((function() t(runif(1, -1, 1))) %::% (list() %->% st("numericScalar")),
+typedSvmKernelConstantSet <- constantFactorySet((function() t(runif(1, -1, 1))) %::% (list() %->% st("numeric")),
                                                 (function() runif(vectorDimension, -1, 1)) %::% (list() %->% st("numericVector")))
 typedSvmKernelInputVariableSet <- inputVariableSet("x" %::% st("numericVector"),
                                                    "y" %::% st("numericVector"))
-typedSvmKernelFunctionSet <- functionSet("exp" %::% (list(st("numericScalar")) %->% st("numericScalar")),
-                                         "+" %::% (list(st("numericScalar"), st("numericScalar")) %->% st("numericScalar")),
-                                         "*" %::% (list(st("numericScalar"), st("numericScalar")) %->% st("numericScalar")),
-                                         "-" %::% (list(st("numericScalar"), st("numericScalar")) %->% st("numericScalar")),
-                                         "/" %::% (list(st("numericScalar"), st("numericScalar")) %->% st("numericScalar")),
+typedSvmKernelFunctionSet <- functionSet("exp" %::% (list(st("numeric")) %->% st("numeric")),
+                                         "+" %::% (list(st("numeric"), st("numeric")) %->% st("numeric")),
+                                         "*" %::% (list(st("numeric"), st("numeric")) %->% st("numeric")),
+                                         "-" %::% (list(st("numeric"), st("numeric")) %->% st("numeric")),
+                                         "/" %::% (list(st("numeric"), st("numeric")) %->% st("numeric")),
                                          "%+%" %::% (list(st("numericVector"), st("numericVector")) %->% st("numericVector")),
                                          "%-%" %::% (list(st("numericVector"), st("numericVector")) %->% st("numericVector")),
-                                         "%*%" %::% (list(st("numericVector"), st("numericVector")) %->% st("numericScalar")),
-                                         "%s*%" %::% (list(st("numericScalar"), st("numericVector")) %->% st("numericVector")),
-                                         "%*s%" %::% (list(st("numericVector"), st("numericScalar")) %->% st("numericVector")),
-                                         "%s^%" %::% (list(st("numericScalar"), st("numericScalar")) %->% st("numericScalar")),
-                                         "%v^%" %::% (list(st("numericVector"), st("numericScalar")) %->% st("numericVector")))
+                                         "%*%" %::% (list(st("numericVector"), st("numericVector")) %->% st("numeric")),
+                                         "%s*%" %::% (list(st("numeric"), st("numericVector")) %->% st("numericVector")),
+                                         "%*s%" %::% (list(st("numericVector"), st("numeric")) %->% st("numericVector")),
+                                         "%s^%" %::% (list(st("numeric"), st("numeric")) %->% st("numeric")),
+                                         "%v^%" %::% (list(st("numericVector"), st("numeric")) %->% st("numericVector")))
 
 
 # Evolution wrapper functions...
@@ -84,16 +84,18 @@ typedSvmKernelFunctionSet <- functionSet("exp" %::% (list(st("numericScalar")) %
 sizeFitnessFunction <- function(f) 0 - funcSize(f) # grow large functions for testing
 
 evolveFeaturePairs <- function(fitnessFunction, stopCondition = makeTimeStopCondition(5))
-  typedGeneticProgramming.r(fitnessFunction, st("numericPair"),
-                            functionSet = typedMathPairFunctionSet, constantSet = typedNumericPairConstantSet,
-                            stopCondition = stopCondition)
+  typedGeneticProgramming(fitnessFunction, st("numericPair"),
+                          functionSet = typedMathPairFunctionSet,
+                          inputVariables = typedNumericInputVariableSet,
+                          constantSet = typedNumericPairConstantSet,
+                          stopCondition = stopCondition)
 
 evolveSvmKernels <- function(fitnessFunction, stopCondition = makeTimeStopCondition(5))
-  typedGeneticProgramming(fitnessFunction, st("numericScalar"),
+  typedGeneticProgramming(fitnessFunction, st("numeric"),
                           functionSet = typedSvmKernelFunctionSet,
                           inputVariables = typedSvmKernelInputVariableSet,
                           constantSet = typedSvmKernelConstantSet,
-                          population = makeTypedPopulation(100, st("numericScalar"),
+                          population = makeTypedPopulation(100, st("numeric"),
                             typedSvmKernelFunctionSet, typedSvmKernelInputVariableSet, typedSvmKernelConstantSet,
                             maxfuncdepth = 3, constprob = 0.1),
                           stopCondition = stopCondition)
