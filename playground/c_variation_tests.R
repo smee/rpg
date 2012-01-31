0e6998760f9f06b3206d962f876483845ff14835
Oliver Flasch <of@sourcewerk.de>
2012-01-31 15:23:27 +0100

    fast untyped mutation operators in cee

--- playground/c_variation_tests.R
+++ playground/c_variation_tests.R
@@ -7,3 +7,5 @@ dyn.load("random_utils.so"); e1 <- .Call("initialize_expression_grow_R", list("+
 
 dyn.load("random_utils.so"); e1 <- .Call("mutate_functions_R", quote(2*x1+log(3.14)), 0.5, list("+", "-", "*", "/", "log", "exp"), c(2L, 2L, 2L, 2L, 1L, 1L)); e1
 
+dyn.load("random_utils.so"); e1 <- .Call("mutate_subtrees_R", quote(2*x1+log(3.14)), 1.0, 0.5, list("+", "-", "*", "/", "log"), c(2L, 2L, 2L, 2L, 1L), list("x1", "x2", "x3"), -1, 1, 1.0, 0.5, 2L); e1
+
--- skel/src/mutation.h
+++ skel/src/mutation.h
@@ -18,8 +18,28 @@ SEXP mutate_constants_normal_R(SEXP sexp, SEXP p, SEXP mu, SEXP sigma);
 /* mutate_subtrees
  *
  */
-SEXP mutate_subtrees(SEXP sexp, double p, double mu, double sigma, SEXP input_variable_list);
-SEXP mutate_subtrees_R(SEXP sexp, SEXP p, SEXP mu, SEXP sigma, SEXP input_variable_list);
+SEXP mutate_subtrees(SEXP sexp,
+                     double p_insert, double p_delete,
+                     SEXP function_symbol_list,
+                     SEXP function_arities,
+                     SEXP input_variable_list,
+                     double constant_min, double constant_max,
+                     double p_subtree, double p_constant,
+                     int depth_max);
+SEXP mutate_subtrees_R(SEXP sexp,
+                       SEXP p_insert, SEXP p_delete,
+                       SEXP function_symbol_list,
+                       SEXP function_arities,
+                       SEXP input_variable_list,
+                       SEXP constant_min, SEXP constant_max,
+                       SEXP p_subtree, SEXP p_constant,
+                       SEXP depth_max); 
+
+/* mutate_functions
+ *
+ */
+SEXP mutate_functions(SEXP sexp, double p, SEXP function_symbol_list, SEXP function_arities);
+SEXP mutate_functions_R(SEXP sexp, SEXP p, SEXP function_symbol_list, SEXP function_arities);
 
 #endif
 
