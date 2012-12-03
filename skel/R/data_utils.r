a8f158b626d6704e78d38d2faf41251da1b9cca5
Oliver Flasch <of@sourcewerk.de>
2012-12-03 13:17:03 +0100

    ...

--- skel/R/data_utils.r
+++ skel/R/data_utils.r
@@ -4,6 +4,7 @@
 ## RGP - a GP system for R
 ## 2010-2012 Oliver Flasch (oliver.flasch@fh-koeln.de)
 ## with contributions of Thomas Bartz-Beielstein, Patrick Koch, Olaf Mersmann and Joerg Stork
+## Latin Hypercube Sampling Code by Chrisitian Lasarczyk
 ## released under the GPL v2
 ##
 
@@ -72,7 +73,7 @@ subDataFrame <- function(x, size = 1.0, pos = "START") {
 ##' @return The regular grid design matrix.
 ##'
 ##' @export
-gridDesign <- function(dimension, points = seq(from=0.0, to=1.0, length.out=10)) {
+gridDesign <- function(dimension, points = seq(from = 0.0, to = 1.0, length.out = 10)) {
   s = length(points)
   D <- matrix(0.0, nrow = s ** dimension, ncol = dimension)
 
@@ -84,4 +85,68 @@ gridDesign <- function(dimension, points = seq(from=0.0, to=1.0, length.out=10))
   }
 
   return (D)
+}
+
+##' Create a normalized design matrix
+##'
+##' Produces a normalized design and calculates the minimal distance 
+##' if required. Returns a design is a matrix with \code{dim} columns and
+##' \code{size} rows.
+##' 
+##' @param dimension Dimension of the problem (will be no. of columns of the result matrix).
+##' @param size Number of points with that dimension needed. (will be no. of rows of the result matrix).
+##' @param calcMinDistance Indicates whether a minimal distance should be calculated.
+##' @return List \code{L} consists of a matrix and nd (if required) a minimal distance.
+##'
+##' @export
+normalizedDesign <- function(dimension, size, calcMinDistance = FALSE) {
+	step <- 1 / size
+	D <- replicate(dimension, sample(0:(size - 1), size) * step + runif(size) * step)
+
+	if (calcMinDistance) {
+		minDistance <- min(dist(D))
+	} else {
+		minDistance <- NA
+  }
+
+	list(design = D,  minDistance = minDistance)
+}
+
+##' Create a latin hypercube design (LHD)
+##'
+##' Produces a LHD matrix with \code{dimension} columns and \code{size}
+##' rows.
+##'
+##' @param dimension Dimension of the problem (will be no. of columns of the result matrix).
+##' @param size Number of design points, defaults to \code{max(11 * dimension,
+##'   1 + 3 * dimension + dimension * (dimension - 1) / 2 + 1)}.
+##' @param bounds Interval (2-element numeric vector) giving the upper and lower bounds
+##'   for sampling, defaults to \code{c(0.0, 1.0)}.
+##' @param retries Number of retries, which is the number of trials to find a design
+##'   with the lowest distance, default is \code{2 * dimension}.
+##' @return A LHD matrix. 
+##'
+##' @export
+latinHypercubeDesign <- function(dimension,
+                                 size = max(11 * dimension, 1 + 3 * dimension + dimension * (dimension - 1) / 2 + 1),
+                                 bounds = c(0.0, 1.0),
+                                 retries = 2 * dimension) {
+	# min distance does not have to be calculated if there is only one try 
+	bestNormalizedD <- normalizedDesign(dimension, size, calcMinDistance = retries > 0)
+	if (retries > 0) {
+		for (i in 1:retries) {
+			candidateD <- normalizedDesign(dimension, size, calcMinDistance = TRUE)
+			## maximize minimal distance
+			if (candidateD$minDistance > bestNormalizedD$minDistance) {
+				bestNormalizedD <- candidateD 
+      }
+		}
+	}
+
+  # scale normalized matrix to bounds
+  lowerBound <- bounds[1]
+  upperBound <- bounds[2]
+  D <- lowerBound + bestNormalizedD$design * (upperBound - lowerBound)
+
+	return (D)
 }
