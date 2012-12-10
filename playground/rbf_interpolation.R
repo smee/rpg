66e21b1124bf70df946008dccd5eb30ac52ed666
Oliver Flasch <of@sourcewerk.de>
2012-12-10 19:57:53 +0100

    ...

--- playground/rbf_interpolation.R
+++ playground/rbf_interpolation.R
@@ -19,34 +19,42 @@ rbfInterpolator <- function(points,
   w <- solve(rbf, rhs)
 
   function(x) {
+    # alternative 1: distance calculation via R's native dist function (complexity O(n^2))...
     xDistances <- as.matrix(dist(rbind(x, points), method = distMethod, p = p))
     fVal <- kernel(xDistances[1,])[-1]
+    # alternative 2: distance caculcation via interpreted code (complexity O(n), but slower in practive)...
+    #xDistances <- apply(points, 1, function(point) as.numeric(dist(rbind(x, point), method = distMethod, p = p)))
+    #fVal <- kernel(xDistances)
+    # ... .
     sumW <- sum(w * fVal)
     if (nrbf) sumW / sum(fval) else sumW 
   }
 }
 
-makeGaussRbfKernel <- function(epsilon) function(r) exp(-(epsilon * r) ^ 2)
+makeMultiquadraticRbfKernel <- function(r0) function(r) sqrt(r^2 + r0^2) 
+makeInverseMultiquadraticRbfKernel <- function(r0) function(r) (r^2 + r0^2)^-0.5 
+makeThinPlateRbfKernel <- function(r0) function(r) r^2 * log(r/r0)
+makeGaussianRbfKernel <- function(r0) function(r) exp(-0.5 * r^2 / r0^2)
 
 
 # test code
-plotFunction <- function(f, ...) {
-  X <- seq(0, 1.0, length = 64)
-  Y <- X
-  Z <- outer(X, Y, Vectorize(function(x, y) f(c(x, y))))
-  persp3d(z = Z, xlab = "x1", ylab = "x2", zlab = "y", ...)
+plotFunction <- function(f, samplesPerDimension = 32, ...) {
+  xs <- seq(0, 1.0, length = samplesPerDimension)
+  ys <- xs 
+  zs <- outer(xs, ys, Vectorize(function(x, y) f(c(x, y))))
+  persp3d(z = zs, xlab = "x1", ylab = "x2", zlab = "y", ...)
 }
 
-testRbf <- function(trueFunction, lhdSize = NA) {
+testRbf <- function(trueFunction, kernel, lhdSize = NA) {
   x1 <- if (is.na(lhdSize)) latinHypercubeDesign(2) else latinHypercubeDesign(2, size = lhdSize)
   y1 <- apply(x1, 1, trueFunction)
-  rbf1 <- rbfInterpolator(x1, y1)
+  rbf1 <- rbfInterpolator(x1, values = y1, kernel = kernel)
   plotFunction(trueFunction, col = "blue", alpha = 1/3,
                main = "RBF Interpolation of 2D Functions", sub = "true (blue) versus interpolated (red)")
   plotFunction(rbf1, add = TRUE, col = "red")
 }
 
-twiddleTestRbf <- function(f, centers) {
+twiddleTestRbf <- function(f, centers, k, r0) {
   trueFunction <- switch(f,
                          "sum" = function(x) x[1] + x[2],
                          "product" = function(x) x[1] * x[2],
@@ -56,7 +64,12 @@ twiddleTestRbf <- function(f, centers) {
                          "rastrigin" = rastriginFunction,
                          "mexican hat" = mexicanHatFunction,
                          "branin" = braninFunction)
-  testRbf(trueFunction, centers)
+  kernel <- switch(k,
+                   "inverse multiquadratic" = makeInverseMultiquadraticRbfKernel(r0),
+                   "multiquadratic" = makeMultiquadraticRbfKernel(r0),
+                   "thin plate" = makeThinPlateRbfKernel(r0),
+                   "gaussian" = makeGaussianRbfKernel(r0))
+  testRbf(trueFunction, kernel, lhdSize = centers)
 }
 
 sphereFunction <- function (x) {
@@ -81,8 +94,8 @@ rastriginFunction <- function (x) {
   return(y)
 }
 mexicanHatFunction <- function (x) {	
-	x1 <- (x[1] - 0.5) * 10
-	x2 <- (x[2] - 0.5) * 10
+	x1 <- (x[1] - 0.5) * 20 
+	x2 <- (x[2] - 0.5) * 20 
 	distance <- sqrt(x1^2 + x2^2)
 	if (distance == 0) # stetig ergaenzen
 		{y<-1}
@@ -97,5 +110,8 @@ braninFunction <- function (x) {
     return(y)	
 }
 
-twiddle(twiddleTestRbf(f, centers), centers = knob(lim = c(2, 64), res = 1, default = 22),
-                                    f = combo("product", "sum", "sphere", "six hump", "rosenbrock", "rastrigin", "mexican hat", "branin"))
+twiddle(twiddleTestRbf(f, centers, kernel, r0), 
+                       r0 = knob(lim = c(0.01, 10.0), res = 0.01, default = 1.2),
+                       kernel = combo("inverse multiquadratic", "multiquadratic", "thin plate", "gaussian"),
+                       centers = knob(lim = c(2, 64), res = 1, default = 22),
+                       f = combo("product", "sum", "sphere", "six hump", "rosenbrock", "rastrigin", "mexican hat", "branin"))
