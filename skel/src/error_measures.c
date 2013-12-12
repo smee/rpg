ded9e35d973b930cc1c50015a11346db40301f2c
Oliver Flasch <of@sourcewerk.de>
2013-12-12 03:12:34 +0100

    c-based error measure now support input vectors of differing length (by r-style recycling of values)

--- skel/src/error_measures.c
+++ skel/src/error_measures.c
@@ -1,39 +1,42 @@
 #include <R.h>
 #include <Rinternals.h>
 
-static double mae(const double *x, const double *y, R_len_t n) {
+static double mae(const double *x, const double *y, R_len_t n_x, R_len_t n_y) {
+    R_len_t n = (n_x > n_y) ? n_x : n_y;
     double sum = 0.0;
     R_len_t i;
     for (i = 0; i < n; ++i) {
-        sum += fabs(x[i] - y[i]);
+        sum += fabs(x[i % n_x] - y[i % n_y]);
     }
     return sum / n;
 }
 
-static double sse(const double *x, const double *y, R_len_t n) {
+static double sse(const double *x, const double *y, R_len_t n_x, R_len_t n_y) {
+    R_len_t n = (n_x > n_y) ? n_x : n_y;
     double sum = 0.0;
     R_len_t i;
     for (i = 0; i < n; ++i) {
-        const double delta = x[i] - y[i];
+        const double delta = x[i % n_x] - y[i % n_y];
         sum += delta * delta;
     }
     return sum;
 }
 
-static double ssse(const double *x, const double *y, R_len_t n) {
+static double ssse(const double *x, const double *y, R_len_t n_x, R_len_t n_y) {
+    R_len_t n = (n_x > n_y) ? n_x : n_y;
     R_len_t i;
     double sum = 0.0, mean_x = 0.0, mean_y = 0.0, var_y = 0.0, cov_xy = 0.0;
     
     for (i = 0; i < n; ++i) {
-        mean_x += x[i];
-        mean_y += y[i];
+        mean_x += x[i % n_x];
+        mean_y += y[i % n_y];
     }
     mean_x /= n;
     mean_y /= n;
 
     for (i = 0; i < n; ++i) {
-        cov_xy += (x[i] - mean_x) * (y[i] - mean_y);
-        var_y += (y[i] - mean_y) * (y[i] - mean_y);
+        cov_xy += (x[i % n_x] - mean_x) * (y[i % n_y] - mean_y);
+        var_y += (y[i % n_y] - mean_y) * (y[i % n_y] - mean_y);
     }
     cov_xy /= (n-1);
     var_y /= (n-1);
@@ -42,7 +45,7 @@ static double ssse(const double *x, const double *y, R_len_t n) {
     const double a = mean_x - b * mean_y;
     
     for (i = 0; i < n; ++i) {
-        const double delta = x[i] - (a + b * y[i]);        
+        const double delta = x[i % n_x] - (a + b * y[i % n_y]);        
         sum += delta * delta;
     }
     return sum;
@@ -67,8 +70,7 @@ static double ssse(const double *x, const double *y, R_len_t n) {
     SEXP N(SEXP s_x, SEXP s_y) {        \
     UNPACK_REAL_VECTOR(s_x, x, n_x);    \
     UNPACK_REAL_VECTOR(s_y, y, n_y);    \
-    int n = (n_x < n_y) ? n_x : n_y;    \
-    double result = F(x, y, n);         \
+    double result = F(x, y, n_x, n_y);  \
     return ScalarReal(result);          \
 }
 
