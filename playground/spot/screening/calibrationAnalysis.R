4e6e4f08d1c75b6897ec2d2851ea54d75ff0b4b7
Oliver Flasch <of@sourcewerk.de>
2013-12-26 10:27:21 +0100

    ...

--- playground/spot/screening/calibrationAnalysis.R
+++ playground/spot/screening/calibrationAnalysis.R
@@ -2,4 +2,8 @@
 #
 
 r1 <- readRDS("salustowicz1d_calibration.RDS")
+
+pdf("gmogp_calibration_fitness_histories.pdf", family = "Palatino", width = 8 ,height = 8)
 matplot(x = r1$fitnessHistories[,1], y = r1$fitnessHistories[,-1], type = "l", xlab = "# Fitness Evaluations", ylab = "Training Fitness (SMSE)")
+dev.off()
+
